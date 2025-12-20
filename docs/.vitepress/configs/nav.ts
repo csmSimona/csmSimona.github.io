cf941d67982e8cc5834e1bd2561ee6315f114034
csmSimona <980715844@qq.com>
2025-12-20 23:45:36 +0800

    test

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -3,7 +3,7 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: "首页", link: "/" },
-  // { text: "导航", link: "" },
+  { text: "导航", link: "" },
   {
     text: "前端汇总",
     items: [
